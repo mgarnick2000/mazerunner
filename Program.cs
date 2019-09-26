d1e99c6d70d16157b6f564b949409c79a3dcc384
Mark <mgarnick@techtonicgroup.com>
2019-09-26 14:38:44 -0600

    setup git

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mazerunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
